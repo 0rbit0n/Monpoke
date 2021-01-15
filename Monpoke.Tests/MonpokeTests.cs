da79eddecb7250dbdb97bceb18d1475f612962dd
Kirill Medvedev <kirill.medvedev@echo.com>
2021-01-15 17:24:33 -0600

    monpoke can be damaged

--- Monpoke.Tests/MonpokeTests.cs
+++ Monpoke.Tests/MonpokeTests.cs
@@ -13,5 +13,18 @@ namespace Monpoke.Tests
 
             monpoke.Id.Should().Be("monpoke");
         }
+
+        [DataTestMethod]
+        [DataRow(5, 3, true,  DisplayName = "Monpoke damaged slightly is alive")]
+        [DataRow(5, 5, false, DisplayName = "Monpoke damaged hard enough is dead")]
+        [DataRow(5, 7, false, DisplayName = "Monpoke damaged very hard is very dead")]
+        public void MonpokeDamagedHardIsDead(int initialHp, int damage, bool shouldBeAlive)
+        {
+            var monpoke = new Monpoke("monpoke", initialHp, attack: 1);
+
+            monpoke.Damage(damage);
+
+            monpoke.IsAlive().Should().Be(shouldBeAlive);
+        }
     }
 }
--- Monpoke/Monpoke.cs
+++ Monpoke/Monpoke.cs
@@ -1,12 +1,29 @@
-﻿namespace Monpoke
+﻿using System;
+
+namespace Monpoke
 {
     public class Monpoke
     {
         public Monpoke(string id, int hp, int attack)
         {
             this.Id = id;
+            this.hp = hp;
+            this.attack = attack;
         }
 
         public string Id { get; }
+
+        public void Damage(int damage)
+        {
+            hp -= damage;
+        }
+
+        public bool IsAlive()
+        {
+            return hp > 0;
+        }
+
+        private int hp;
+        private int attack;
     }
 }
