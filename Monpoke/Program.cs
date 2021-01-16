fa8f22921cacd63fa491be956a3d2ad861e89a29
Kirill Medvedev <kirill.medvedev@echo.com>
2021-01-15 18:56:07 -0600

    program complete

--- Monpoke/Program.cs
+++ Monpoke/Program.cs
@@ -1,9 +1,26 @@
-﻿namespace Monpoke
+﻿using Monpoke.Commands;
+using System.Linq;
+
+namespace Monpoke
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var inputReader = (IInputReader)null;
+
+            if (args?.Any() ?? false)
+                inputReader = new FileInputReader(args.First());
+            else
+                inputReader = new ConsoleInputReader();
+
+            var output = new Output();
+            var commandFactory = new CommandFactory(output);
+            var game = new Game(output);
+
+            var controller = new GameController(inputReader, commandFactory, game);
+
+            controller.PlayGame();
         }
     }
 }
